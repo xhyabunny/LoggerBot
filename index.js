ea27efd946ad613e083ba2f9ca18cc1f3d9025a8
xhya <106491722+xhyabunny@users.noreply.github.com>
2022-12-16 19:25:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -25,43 +25,43 @@ client.on('ready', () => {
 
 setInterval((function(){
 
-        var index2 = index1[Math.floor(Math.random() * index1.length)]; //random status
-        var index3 = index0[Math.floor(Math.random() * index0.length)]; //random activity type
+    var index2 = index1[Math.floor(Math.random() * index1.length)]; //random status
+    var index3 = index0[Math.floor(Math.random() * index0.length)]; //random activity type
 
-        client.user.setPresence({
-            status: index2,//    <<<< randomized status [index2]
-            activity: {
-                name: 'your mom',
-                type: index3,//  <<<< randomized activity type [index3]
-                url: 'https://www.twitch.tv/ttv_bonndubz'
-            }
-        })
-        
-    }), 1000)
+    client.user.setPresence({
+        status: index2,//    <<<< randomized status [index2]
+        activity: {
+            name: 'your mom',
+            type: index3,//  <<<< randomized activity type [index3]
+            url: 'https://www.twitch.tv/ttv_bonndubz'
+        }
+    })
+}), 1000)
     
-  console.log(`Logged in as ${client.user.tag}!`); //checks connection along with user tag in the prompt
+console.log(`Logged in as ${client.user.tag}!`); //checks connection along with user tag in the prompt
     
 });
 
 client.on('message', message => {
-    
-if(message.content !== null){ // THE LOGGER (finally)
 
-    if(message.author.id === client.user.id){ //if its the bot
-        
-        logger.debug(
-            " BOT SENT - In Server: " + message.member.guild.name + " (" + message.member.lastMessage.guild.id +
-            ") - Channel: #" + message.member.lastMessage.channel.name + " ("+message.member.lastMessage.channel.id+
-            ") " + message.member.user.tag + message.member.user.id +" Messaged: '" + message.member.user.lastMessage.content + "'");
-        
-    }else{                                    //if its not the bot
-        
-        logger.debug(
-            " MESSAGE SENT - In Server: " + message.member.guild.name + " (" + message.member.lastMessage.guild.id +
-            ") - Channel: #" + message.member.lastMessage.channel.name + " ("+message.member.lastMessage.channel.id+
-            ") " + message.member.user.tag + message.member.user.id +" Messaged: '" + message.member.user.lastMessage.content + "'");
-        
+    if(message.content !== null){ // THE LOGGER (finally)
+
+        if(message.author.id === client.user.id){ //if its the bot
+
+            logger.debug(
+                " BOT SENT - In Server: " + message.member.guild.name + " (" + message.member.lastMessage.guild.id +
+                ") - Channel: #" + message.member.lastMessage.channel.name + " ("+message.member.lastMessage.channel.id+
+                ") " + message.member.user.tag + message.member.user.id +" Messaged: '" + message.member.user.lastMessage.content + "'");
+
+        }else{                                    //if its not the bot
+
+            logger.debug(
+                " MESSAGE SENT - In Server: " + message.member.guild.name + " (" + message.member.lastMessage.guild.id +
+                ") - Channel: #" + message.member.lastMessage.channel.name + " ("+message.member.lastMessage.channel.id+
+                ") " + message.member.user.tag + message.member.user.id +" Messaged: '" + message.member.user.lastMessage.content + "'");
+
+        }
     }
-}
+});
 
 client.login(token); //run the bot
